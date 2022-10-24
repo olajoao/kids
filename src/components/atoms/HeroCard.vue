b6ed8c9368d39798eec796edc32dd2ebd64320ee
Joao Kuster <kusterzito@gmail.com>
2022-10-23 23:26:35 -0300

    feat: hero section

---
+++ src/components/atoms/HeroCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="rounded-xl p-5 shadow-md flex flex-col justify-between"
+    :class="[card?.colors]"
+  >
+    <h1 class="title-default">
+      {{ card?.text }}
+    </h1>
+    <router-link to="/" class="cta-default">
+      <span> {{ ctaText }} </span>
+      <ArrowLongRightIcon class="w-5 h-5 lg:w-6 lg:h-6 mt-1" />
+    </router-link>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ArrowLongRightIcon } from '@heroicons/vue/24/outline'
+
+interface Card {
+  text: string
+  colors: string
+}
+
+export default defineComponent({
+  components: { ArrowLongRightIcon },
+  props: {
+    card: { type: Object as PropType<Card>, required: true },
+    ctaText: String
+  }
+})
+</script>
---
+++ src/components/organisms/HeroSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="bg__hero 2xl:h-[50vh] w-full">
+    <div class="bg-main-white/90 w-full h-full">
+      <div class="container mx-auto flex py-10 gap-x-10 h-full">
+        <HeroCard
+          class="w-full max-w-md"
+          :card="{
+            text: 'Lorem ipsum dolor sit amet consectum',
+            colors: 'bg-main-gray shadow-dark-brown/30  text-dark-brown'
+          }"
+          ctaText="Find course"
+        />
+        <HeroCard
+          class="flex-1"
+          :card="{
+            text: 'How i met your mother',
+            colors: 'bg-dark-gray  shadow-dark-brown/30  text-main-white'
+          }"
+          ctaText="Discover everything"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HeroCard from '../atoms/HeroCard.vue'
+
+export default defineComponent({
+  components: { HeroCard }
+})
+</script>
+<style scoped>
+.bg__hero {
+  background: url('@/assets/hero-bg.png');
+  background-size: 40%;
+}
+</style>
